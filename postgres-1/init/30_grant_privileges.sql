c2320bda154b88898759943376d98ef453e15cad
Peacock El Gap <peacock.el.gap@gmail.com>
2022-02-12 21:54:15 +0100

    Update: comment added

--- postgres-1/init/30_grant_privileges.sql
+++ postgres-1/init/30_grant_privileges.sql
@@ -3,6 +3,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA keycloak_schema
 TO keycloak_user;
 
+-- This user is granted CREATE privilege - only for Phase 1
 GRANT CREATE
 ON SCHEMA keycloak_schema
 TO keycloak_user;
